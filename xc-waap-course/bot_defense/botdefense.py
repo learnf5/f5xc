fd1c258668ffda8abb34c19152a12fc9d4e24de4
Pawel F5 <p.kuligowski@f5.com>
2022-12-05 11:00:49 +0000

    Added login logic

--- xc-waap-course/bot_defense/botdefense.py
+++ xc-waap-course/bot_defense/botdefense.py
@@ -1,11 +1,17 @@
 """Juice Shop Bot defense test v1.1 P.Kuligowski 9/9/2022"""
-
 import sys
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException
+import sys
+
+
+if len(sys.argv) != 2:
+    print("usage: " + sys.argv[0] + " <JuiceShop-FQDN>")
+    sys.exit()
 
 
 url = "https://" + sys.argv[1] + "/#/login"
@@ -42,12 +48,17 @@ def main(credentials):
             element = browser.find_element(by=By.XPATH, value='//*[@id="loginButton"]/span[1]')
             browser.execute_script("arguments[0].click();", element)
             time.sleep(1.0)
-            title = browser.title          
-            if 'Account Information' in title:
-                print('Login Success!')
-            else:
-                print('Login Failure!')
-            time.sleep(2)
+            try:
+                browser.find_element(by=By.XPATH, value='/html/body/app-root/div/mat-sidenav-container/mat-sidenav-content/app-search-result/div/div/div[2]/mat-grid-list/div/mat-grid-tile[1]/div/mat-card/div[1]/div[1]/img')
+                time.sleep(1.0)
+                print("FOUND THE PASSWORD! BREAKING")
+                time.sleep(20)
+                break
+            except NoSuchElementException:
+                print("Wrong Password!")
+                time.sleep(1.0)
+        
+            
     except KeyboardInterrupt:
         print('---------------------------------------------------')
         print("Bye!")
--- xc-waap-course/credstuffer/credstuffer.py
+++ xc-waap-course/credstuffer/credstuffer.py
@@ -1,9 +1,10 @@
-#! /usr/bin/env python3
+#!/usr/bin/python3
 # Created on Oct 28, 2020
 # F5 Global Services Training
 
 import sys
 import requests
+
 
 if len(sys.argv) != 2:
   print("usage: " + sys.argv[0] + " <FQDN> <username>")
