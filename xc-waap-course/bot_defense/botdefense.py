112aef05978c030f0979902d34a311ad4c62bd62
pavbjj <43717447+pavbjj@users.noreply.github.com>
2022-12-10 09:08:28 +0100

    Update botdefense.py

--- xc-waap-course/bot_defense/botdefense.py
+++ xc-waap-course/bot_defense/botdefense.py
@@ -6,7 +6,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import NoSuchElementException
-import sys
 
 
 if len(sys.argv) != 2:
