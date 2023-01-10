d63584b8ac0dd92c83e346add03c6d7b62c04aa3
pavbjj <43717447+pavbjj@users.noreply.github.com>
2023-01-10 10:21:25 +0100

    Update baseline_traffic.sh
    Lowered the request rate so we don't hit the API endpoint quota.

--- xc-waap-course/baseline_traffic_gen/baseline_traffic.sh
+++ xc-waap-course/baseline_traffic_gen/baseline_traffic.sh
@@ -28,20 +28,10 @@ IP=$1
 # $3 Source Address 2
 # $4 Source Address 3
 SRC_ADDR1=$(ip a show dev eth1 | grep inet |grep -v inet6| awk -F'[/ ]+' '{print $3}')
-SRC_ADDR2=$(ip a show dev eth1 | grep inet |grep -v inet6| awk -F'[/ ]+' '{print $3}')
-SRC_ADDR3=$(ip a show dev eth1 | grep inet |grep -v inet6| awk -F'[/ ]+' '{print $3}')
 #
-###################################
-#IP=$1
-#IP="10.10.${student_id}.100"
-#if [ "$IP" == "" ]
-# then
-#        #echo -n "Enter Target IP as an Argument"
-#        #exit
-#fi
 
 BASELINE='Please enter your type of baselining: '
-options=("increasing" "alternate" "Quit")
+options=("increasing" "Quit")
 select opt in "${options[@]}"
 do
         case $opt in
@@ -52,39 +42,9 @@ do
                                 echo
                                 for i in $(eval echo "{0..`date +%M`}")
                                         do
-                                                curl -0 --interface $SRC_ADDR1 -s -o /dev/null -A "`shuf -n 1 useragents_with_bots.txt`" -w "status: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" https://$IP`shuf -n 1 urls.txt`
-                                                curl -0 --interface $SRC_ADDR2 -s -o /dev/null -A "`shuf -n 1 useragents_with_bots.txt`"  https://$IP`shuf -n 1 urls.txt`
-                                                curl -0 --interface $SRC_ADDR3 -s -o /dev/null -A "`shuf -n 1 useragents_with_bots.txt`"  https://$IP`shuf -n 1 urls.txt`
-                                                #curl -0 -s -o /dev/null -A "`sort -R useragents_with_bots.txt | head -1`" -w "status: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" http://$IP`sort -R urls.txt | head -1`
-                                        done
-                                #sleep 0.1
-                        done
-                ;;
-                "alternate")
-                        while true; do
-                                clear
-                                echo "Hourly alternate traffic: $IP"
-                                echo
-                                #if (( {`date +%k` % 2} )); then
-                                if (( `date +%k` % 2 )); then
-                                        for i in {1..100};
-                                                do
-                                                        curl -0 --interface $SRC_ADDR1 -A -s -o /dev/null -A "`shuf -n 1 useragents_with_bots.txt`" -w "High:\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" https://$IP`shuf -n 1 urls.txt`
-                                                        curl -0 --interface $SRC_ADDR2 -A -s -o /dev/null -A "`shuf -n 1 useragents_with_bots.txt`" https://$IP`shuf -n 1 urls.txt`
-                                                        curl -0 --interface $SRC_ADDR3 -A -s -o /dev/null -A "`shuf -n 1 useragents_with_bots.txt`" https://$IP`shuf -n 1 urls.txt`
-                                                        #curl -0 -s -o /dev/null -A "`sort -R useragents_with_bots.txt | head -1`" -w "status: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" http://$IP`sort -R urls.txt | head -1`
+                                                curl -0 --interface $SRC_ADDR1 -s -o /dev/null -A "`shuf -n 1 useragents_with_bots.txt`" -w "status: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" https://$IP`shuf -n 1 urls.txt`   
                                                 done
-                                else
-                                        for i in {1..50};
-                                                do
-                                                        curl --interface $SRC_ADDR1 -s -o /dev/null -A "`shuf -n 1 useragents_with_bots.txt`" -w "High:\tstatus: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" https://$IP`shuf -n 1 urls.txt`
-                                                        curl --interface $SRC_ADDR2 -s -o /dev/null -A "`shuf -n 1 useragents_with_bots.txt`"  https://$IP`shuf -n 1 urls.txt`
-                                                        curl --interface $SRC_ADDR3 -s -o /dev/null -A "`shuf -n 1 useragents_with_bots.txt`"  https://$IP`shuf -n 1 urls.txt`
-                                                        #curl -0 -s -o /dev/null -A "`sort -R useragents_with_bots.txt | head -1`" -w "status: %{http_code}\tbytes: %{size_download}\ttime: %{time_total}\n" http://$IP`sort -R urls.txt | head -1`
-                                                done
-                                fi
-                                #sleep 0.1
-                                clear
+                                sleep 1
                         done
                 ;;
                 "Quit")
