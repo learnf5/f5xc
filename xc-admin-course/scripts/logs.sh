1e1ea7b271710d1524b8b8fc805ca0e35e44eb56
gsmythf5 <149519677+gsmythf5@users.noreply.github.com>
2024-11-29 14:41:01 +0000

    Update logs.sh

--- xc-admin-course/scripts/logs.sh
+++ xc-admin-course/scripts/logs.sh
@@ -34,18 +34,19 @@ echo ""
 echo "Options:"
 echo ""
 echo "-tok                       Test token"
-echo "-adcheck <sitename>        ADMIN Check if logs exist for sitename"
-echo "-adstart <sitename>        ADMIN Start log collection on site"
-echo "-adlogs <sitename>         ADMIN Get logs for sitename"
+echo "-adcheck <studentname>     ADMIN Check if logs exist for sitename"
+echo "-adstart <studentname>     ADMIN Start log collection on site"
+echo "-adlog <studentname>       ADMIN Get (a) log for sitename"
+echo "-adzip <studentname>       ** API does not work ** ADMIN Get the zip file of all logs for sitename"
 echo ""
 echo "See https://docs.cloud.f5.com/docs-v2/api/operate-debug"
-echo ""
-echo "Unfortunately the APIs mix sitename and node and are not clear what they refer to"
 echo ""
 echo "sitename is the name of the F5XC vpc object, studentX-vpc for example"
 echo "nodename is NOT the CE cluster kubernetes nodename, its the XC nodename, found inside"
 echo "Multi-Cloud Network Connect > Overview > Infrastructure > Sites > student3-vpc > Metrics > Nodes"
-echo "For student3 that name is ip-172-31-3-5"
+echo "For student3 that name is ip-172-31-3-5 which is a form of the private IPv4 AWS address"
+echo ""
+echo "Logs have to be first started, then collected. Old logs won't be collected after an hour"
 echo ""
 exit 0
 }
@@ -68,11 +69,18 @@ s_nodename="ip-172-31-3-54"
 curl -s -H "Content-Type:application/json" -H "Authorization: APIToken $v_token" -X GET "$v_url/operate/namespaces/system/sites/$s_sitename/vpm/debug/$s_nodename/start-debug-info-collection" | jq
 }
 
-f_admin_get_logs()
+f_admin_get_log()
 {
-s_cename="$1-vpc"
+s_sitename="$1-vpc"
 s_nodename="ip-172-31-3-54"
-curl -s -H "Content-Type:application/json" -H "Authorization: APIToken $v_token" -X GET "$v_url/operate/namespaces/system/sites/$s_cename/vpm/debug/$s_nodename/vpm/log" | jq
+curl -s -H "Content-Type:application/json" -H "Authorization: APIToken $v_token" -X GET "$v_url/operate/namespaces/system/sites/$s_sitename/vpm/debug/$s_nodename/vpm/log" | jq
+}
+
+f_admin_get_zip()
+{
+s_sitename="$1-vpc"
+s_nodename="ip-172-31-3-54"
+curl -s -H "Content-Type:application/json" -H "Authorization: APIToken $v_token" -X GET "$v_url/operate/namespaces/system/sites/$s_sitename/vpm/debug/global/download-debug-info-collection"
 }
 
 ### main
@@ -101,13 +109,21 @@ while [ $# -gt 0 ]; do
    f_echo "Starting log collection for $2 ..."
    f_admin_start_logs $2
    ;;
-   -adlogs)
+   -adlog)
    if [ "$#" != 2 ]; then
     f_echo "Missing student name ... "
     exit 1
    fi
-   f_echo "Getting CE logs for $2 ..."
+   f_echo "Getting CE log for $2 ..."
    f_admin_get_logs $2
+   ;;
+   -adzip)
+   if [ "$#" != 2 ]; then
+    f_echo "Missing student name ... "
+    exit 1
+   fi
+   f_echo "Getting zip file of logs for $2 ..."
+   f_admin_get_zip $2
    ;;
    *)
    ;;
