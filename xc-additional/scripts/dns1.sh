17d06b0c4b0b37e2908b08b81cc3a7be7944e76c
gsmythf5 <149519677+gsmythf5@users.noreply.github.com>
2025-11-27 16:28:42 +0000

    Update dns1.sh

--- xc-additional/scripts/dns1.sh
+++ xc-additional/scripts/dns1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ### variables
-v_token=""
+v_token="M6KjxIsIhxqzyNxKI6wUe36Kt+U="
 v_url="training1.console.ves.volterra.io/api"
 
 ### Test the token
@@ -13,6 +13,7 @@ v_url="training1.console.ves.volterra.io/api"
 ### Get the Zone
 ### curl -s -H "Authorization: APIToken $v_token" -X GET "https://$v_url/config/dns/namespaces/system/dns_zones/f5training1.cloud"
 ### sleep 2
-### Add one record
-### curl -s -H "Authorization: APIToken $v_token" -X GET "https://$v_url/config/dns/namespaces/system/dns_zones/f5training1.cloud/rrsets/x-ves-io-managed" -d '{"dns_zone_name":"f5training1.cloud","group_name":"x-ves-io-managed","rrset":{"a_record":{"name":"record77","values":["10.10.10.150"]}},"description":"Testing RR Update"}'
-curl -v -s -H "Authorization: APIToken $v_token" -X POST "https://$v_url/config/dns/namespaces/system/dns_zones/f5training1.cloud/rrsets/mygroup1" -d '{"dns_zone_name":"f5training1.cloud","group_name":"mygroup1","rrset":{"ttl":3600,"a_record":{"name":"record77","values":["10.10.10.150"]},"description":"Testing RR Update"}}'
+### Add one record to existing group
+### curl -v -s -H "Authorization: APIToken $v_token" -X POST "https://$v_url/config/dns/namespaces/system/dns_zones/f5training1.cloud/rrsets/mygroup1" -d '{"dns_zone_name":"f5training1.cloud","group_name":"mygroup1","rrset":{"ttl":3600,"a_record":{"name":"record77","values":["10.10.10.150"]},"description":"Testing RR Update"}}'
+### Try F5 managed
+curl -v -s -H "Authorization: APIToken $v_token" -X POST "https://$v_url/config/dns/namespaces/system/dns_zones/f5training1.cloud/rrsets/x-ves-io-managed" -d '{"dns_zone_name":"f5training1.cloud","group_name":"x-ves-io-managed","rrset":{"ttl":3600,"a_record":{"name":"record222","values":["10.10.10.160"]},"description":"Testing RR Update"}}'
