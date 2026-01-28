b0671daaa30ab2d6fab1e09553458f61d3ea4f01
gsmythf5 <149519677+gsmythf5@users.noreply.github.com>
2026-01-28 11:25:56 +0000

    Create vars.tf

---
+++ xc-additional/terraform/lab4/vars.tf
@@ -0,0 +1,15 @@
+variable "my_namespace" {
+  type    = string
+}
+
+variable "my_known_label_key" {
+  type    = string
+}
+
+variable "my_known_label" {
+  type    = string
+}
+
+variable "my_virtual_site" {
+  type    = string
+}
