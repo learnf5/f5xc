a9791fe909ed2f5ff5e421dcd865efdc69b62128
dmonye017 <monye.test@gmail.com>
2026-09-11 17:39:48 +0100

    Increase create_duration for student resources

--- v2.5/xc-admin-course/terraform/smsv2/existing/f5xc-smsv2.tf
+++ v2.5/xc-admin-course/terraform/smsv2/existing/f5xc-smsv2.tf
@@ -7,7 +7,7 @@ resource "time_sleep" "site_ready" {
 
   depends_on = [volterra_securemesh_site_v2.student]
 
-  create_duration = "30s"
+  create_duration = "50s"
 }
 
 resource "volterra_token" "student" {
@@ -25,7 +25,7 @@ resource "time_sleep" "token_ready" {
 
   depends_on = [volterra_token.student]
 
-  create_duration = "10s"
+  create_duration = "30s"
 }
 
 # ============================================================
