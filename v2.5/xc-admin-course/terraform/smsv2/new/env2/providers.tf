2b13ce91d3e412561f88c8e49eae16674fb247fb
dmonye017 <monye.test@gmail.com>
2026-09-11 11:21:08 +0100

    Add Terraform provider configurations for AWS and Volterra

--- v2.5/xc-admin-course/terraform/smsv2/new/env2/providers.tf
+++ v2.5/xc-admin-course/terraform/smsv2/new/env2/providers.tf
@@ -1,1 +1,35 @@
+terraform {
+  required_version = ">= 1.0"
 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    volterra = {
+      source  = "volterraedge/volterra"
+      version = "~> 0.11"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.12"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+provider "volterra" {
+  api_p12_file = var.f5xc_api_p12_file
+  url          = var.f5xc_api_url
+}
